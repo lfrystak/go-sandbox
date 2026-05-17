612c642154b233e328f9d0b8602432d7472fe429
lfrystak <88457688+lfrystak@users.noreply.github.com>
2026-05-17 08:53:13 +0200

    feat: Improve user interface
    I just rearranged the messages.

--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World! Greetings from go-sandbox.")
+    fmt.Println("Hello, World!")
+    fmt.Println("Greetings from go-sandbox.")
 }
